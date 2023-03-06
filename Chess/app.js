bb53f27ddfd0236a8e76009e810877279e97812b
AnishSingh15 <anish15may@gmail.com>
2023-03-07 02:29:53 +0530

    cursor animation

--- Chess/app.js
+++ Chess/app.js
@@ -1,3 +1,56 @@
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#31005c", 
+  "#53005f", 
+  "#6f0060", 
+  "#890b60", 
+  "#a01f5f", 
+  "#b5325e", 
+  "#c7475d", 
+  "#d75c5c", 
+  "#e4715d", 
+  "#ef885f", 
+  "#f89e64", 
+  "#ffb56b"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
 
 const mapData = {
   minX: 0,
--- Jungle/app.js
+++ Jungle/app.js
@@ -1,3 +1,57 @@
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#035c00", 
+  "#256600", 
+  "#3c7100", 
+  "#517b00", 
+  "#658503", 
+  "#7a8f09", 
+  "#8f9911", 
+  "#a4a319", 
+  "#baac23", 
+  "#d1b52d", 
+  "#e8be38", 
+  "#ffc744"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
 const mapData = {
   minX: 0,
   maxX: 16,
